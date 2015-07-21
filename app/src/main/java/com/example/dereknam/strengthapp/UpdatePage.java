f6cdf78bb3046054a597a81f612245aa518af5e1
500poundbear <namjjd@gmail.com>
2015-07-21 11:37:23 +0800

    Inefficient way of querying latest status per list view row

--- app/src/main/java/com/example/dereknam/strengthapp/UpdatePage.java
+++ app/src/main/java/com/example/dereknam/strengthapp/UpdatePage.java
@@ -3,16 +3,30 @@ package com.example.dereknam.strengthapp;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class UpdatePage extends ActionBarActivity implements NumberPicker.OnValueChangeListener {
+    private ArrayList<String> listIds;
+    private ArrayList<Boolean> listAvailability;
+    private ArrayList<Long> listStatusLabelId;
+    private IndividualsDataSource dataSource;
+    private StatusesDataSource statusesDataSource;
+    ItemArrayAdapter adapter;
+
+
     public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
         final NumberPicker np = (NumberPicker) findViewById(R.id.np);
         final int nVal = newVal;
@@ -31,50 +45,65 @@ public class UpdatePage extends ActionBarActivity implements NumberPicker.OnValu
         setContentView(R.layout.activity_update_page);
 
         Intent intent=getIntent();
-        int position = Integer.parseInt(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
+        long groupId = Long.parseLong(intent.getStringExtra("GROUP_ID"));
 
-        Toast.makeText(getApplicationContext(), Integer.toString(position), Toast.LENGTH_SHORT);
+        dataSource = new IndividualsDataSource(this);
+        statusesDataSource = new StatusesDataSource(this);
+        try{
+            dataSource.open();
+            statusesDataSource.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        final ArrayList<Individual> values = dataSource.getIndividualsByGroupId(groupId);
+
+        TextView currentTally = (TextView)findViewById(R.id.current_tally);
+        TextView currentTotal = (TextView)findViewById(R.id.current_total);
+
+        currentTally.setText("Currently: x/"+values.size());
+        currentTotal.setText(String.valueOf(values.size()));
 
         final NumberPicker np = (NumberPicker) findViewById(R.id.np);
         np.post(new Runnable() {
             @Override
             public void run() {
 
-                np.setMaxValue(5);
+                np.setMaxValue(values.size());
                 np.setMinValue(0);
                 np.setWrapSelectorWheel(false);
-                np.setValue(5);
-            }
-        });
-        final NumberPicker np2 = (NumberPicker) findViewById(R.id.np2);
-        np2.setOnValueChangedListener(this);
-
-        np2.post(new Runnable() {
-            @Override
-            public void run() {
-
-                np2.setMaxValue(100);
-                np2.setMinValue(0);
-                np2.setWrapSelectorWheel(false);
-                np2.setValue(5);
+                np.setValue(0);
             }
         });
 
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("Hairy Godzilla");
-        list.add("item2");
-        list.add("Super Long Name Name Name here");
-        list.add("item2");
-        list.add("item1");
-        list.add("item2");
-        list.add("item1");
-        list.add("item2");
+        listIds = new ArrayList<String>();
+        for(Individual x:values){
+            listIds.add(String.valueOf(x.getId()));
+        }
+
         //instantiate custom adapter
-        ItemArrayAdapter adapter = new ItemArrayAdapter(list, this);
+        adapter = new ItemArrayAdapter(listIds, this);
 
         //handle listview and assign adapter
         ListView lView = (ListView)findViewById(R.id.listDetail);
         lView.setAdapter(adapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),String.valueOf(position)+" id: "+String.valueOf(id),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        listAvailability = new ArrayList<Boolean>();
+        listStatusLabelId = new ArrayList<Long>();
+        for(Individual x:values){
+            //With ID, query latest status
+            Status latestStatus = statusesDataSource.getLatestStatus(x.getId());
+            listAvailability.add(latestStatus.getAccounted());
+            listStatusLabelId.add(latestStatus.getStatus_label_id());
+        }
     }
 
     @Override
@@ -97,5 +126,15 @@ public class UpdatePage extends ActionBarActivity implements NumberPicker.OnValu
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void updateStatuses(View view){
+        ListView lView = (ListView)findViewById(R.id.listDetail);
+
+        listIds.set(0,String.valueOf(2));
+        adapter.notifyDataSetChanged();
+        for(String x:listIds){
+
+        }
+
     }
 }
